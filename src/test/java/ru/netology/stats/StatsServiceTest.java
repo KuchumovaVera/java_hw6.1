3eab53ee471c53003fe7a97c65a80e4308b8bd35
Kuchumova Vera <weryn4ik86@gmail.com>
2022-05-27 17:21:22 +0300

    first commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void shouldSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.sum(sales) / sales.length;
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMax() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.maxSales(sales);
+        long expected = 8;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldMin() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.minSales(sales);
+        long expected = 9;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAboveAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.aboveAvg(sales);
+        long expected = 5;
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldLessAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.lessAvg(sales);
+        long expected = 5;
+        Assertions.assertEquals(actual, expected);
+    }
+}
